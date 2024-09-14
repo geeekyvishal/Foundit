c5449260c1e15a5814b5048e4e6920cb48557663
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-09-14 14:00:28 +0530

    auto delete after 1 week

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const cron = require('node-cron');
 
+// Define the schema
 const UserSchema = new mongoose.Schema({
     googleId: {
         type: String,
@@ -35,4 +37,20 @@ const UserSchema = new mongoose.Schema({
     ]
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+cron.schedule('*/1 * * *', async () => { 
+    const oneWeekAgo = new Date(Date.now() -  7 * 24 * 60 * 60 * 1000); 
+
+    try {
+        await User.updateMany(
+            {}, 
+            { $pull: { items: { date: { $lt: oneWeekAgo } } } } 
+        );
+        console.log('Expired items (older than 1 week) removed');
+    } catch (err) {
+        console.error('Error removing expired items:', err);
+    }
+});
+
+module.exports = User;
